b489c29395b9503eb7e673ab4469aaefb8737b6f
Robert Bartlensky <bartlensky.robert@gmail.com>
2017-10-19 15:53:00 +0100

    Record time it takes to allocate/deallocate.

--- test_proj/main.cpp
+++ test_proj/main.cpp
@@ -1,39 +1,73 @@
+#include <vector>
+#include <ctime>
+#include <fstream>
+
 #include "src/SomeObject.h"
 #include "src/SomeObject2.h"
 #include "src/SomeObject3.h"
+
 #define BOUND 100000
 
-#include <vector>
-
 int main() {
+    std::clock_t start;
+    std::ofstream f("time_taken.txt");
+    f << "Allocating " << BOUND << " objects." << std::endl;
 
+    start = std::clock();
     // let's create a lot of objects!
     std::vector<SomeObject*> objs;
+    for (int i = 0; i < BOUND; ++i) {
+        objs.push_back(new SomeObject());
+    }
+    f << "Allocate SomeObject1: "
+      << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000)
+      << " ms" << std::endl;
+
+    start = std::clock();
     std::vector<SomeObject2*> objs2;
+    for (int i = 0; i < BOUND; ++i) {
+        objs2.push_back(new SomeObject2());
+    }
+    f << "Allocate SomeObject2: "
+      << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000)
+      << " ms" << std::endl;
+
+    start = std::clock();
     std::vector<SomeObject3*> objs3;
     for (int i = 0; i < BOUND; ++i) {
-        // for each SomeObject, there is a 16 bytes overhead -> 16000 bytes of overhead!
-        objs.push_back(new SomeObject());
-        // for 2 SomeObject2, there is a 16 bytes overhead -> 8000 bytes overhead
-        // + sizeof(vector<void*>) + sizeof(map<void*, size_t>) + 8000 * sizeof(pair<void*, size_t>)
-        // in this case, SomeObject2 is consuming even more memory, but these objects only get allocated
-        // in pools of two objects, my assumption is:
-        // if the pool is large enough, then the pool overhead together with the allocation overhead
-        // should be a lot smaller then the normal allocation overhead
-        objs2.push_back(new SomeObject2());
-
         objs3.push_back(new SomeObject3());
     }
+    f << "Allocate SomeObject3: "
+      << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000)
+      << " ms" << std::endl;
 
     // now let's deallocate all of them
+    start = std::clock();
     for (int i = 0; i < BOUND; ++i) {
         delete objs.back();
         objs.pop_back();
+    }
+    f << "Deallocate SomeObject1: "
+      << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000)
+      << " ms" << std::endl;
+
+    start = std::clock();
+    for (int i = 0; i < BOUND; ++i) {
         delete objs2.back();
         objs2.pop_back();
+    }
+    f << "Deallocate SomeObject2: "
+      << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000)
+      << " ms" << std::endl;
+
+    start = std::clock();
+    for (int i = 0; i < BOUND; ++i) {
         delete objs3.back();
         objs3.pop_back();
     }
+    f << "Deallocate SomeObject3: "
+      << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000)
+      << " ms" << std::endl;
 
     return 0;
 }
