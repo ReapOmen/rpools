d6e28bc3d8653cf646dd7f53d1edbc495875b233
Robert Bartlensky <bartlensky.robert@gmail.com>
2017-12-04 22:40:44 +0000

    Add tests for GlobalLinkedPool and custom new.

---
+++ pool_impl/tests/unit_test/test_custom_new_delete.cpp
@@ -0,0 +1,64 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <vector>
+using std::vector;
+
+#include "custom_new_delete.h"
+using efficient_pools::PoolHeaderG;
+
+TEST_CASE("Allocations between 1 and 128 bytes use GlobalLinkedPool",
+          "[custom_new_delete]") {
+    for (size_t i = 0; i <= 120; i += 8) {
+        void* lastPtr = nullptr;
+        // allocations are done in pools of multiples of 8
+        // 1-8 -> 8
+        // 9-16 -> 16
+        // etc.
+        for (size_t j = i + 1; j <= i + 8; ++j) {
+            void* ptr = custom_new_no_throw(j);
+            if (!lastPtr) {
+                lastPtr = ptr;
+            } else {
+                REQUIRE((GlobalLinkedPool::POOL_MASK & (size_t)lastPtr) ==
+                        (GlobalLinkedPool::POOL_MASK & (size_t)ptr));
+
+                const auto& hLast = GlobalLinkedPool::getPoolHeader(lastPtr);
+                const auto& h = GlobalLinkedPool::getPoolHeader(ptr);
+                REQUIRE(h == hLast);
+            }
+        }
+    }
+}
+
+TEST_CASE("Allocations over 128 bytes use malloc",
+          "[custom_new_delete]") {
+    // the uperbound can be anything
+    for (size_t i = 129; i < 1000; ++i) {
+        void* ptr = custom_new_no_throw(i);
+        REQUIRE(strcmp(GlobalLinkedPool::getPoolHeader(ptr).isPool,
+                       PoolHeaderG::IS_POOL) != 0);
+    }
+}
+
+TEST_CASE("Pointers to objects of size < 129 are deallocated using GlobalLinkedpool",
+          "[custom_new_delete]") {
+    std::vector<void*> allocs(128);
+    for (size_t i = 1; i <= 128; ++i) {
+        allocs[i-1] = custom_new_no_throw(i);
+    }
+    for (size_t i = allocs.size() - 1; i != 0; --i) {
+        // we do not want to deal with dangling references
+        // because our pool header will get destroyed once we
+        // deallocate the last object of the pool
+        if ((i - 1) % 8 == 0) {
+            custom_delete(allocs[i]);
+        } else {
+            size_t oldSize =
+                GlobalLinkedPool::getPoolHeader(allocs[i]).sizeOfPool;
+            custom_delete(allocs[i]);
+            REQUIRE(oldSize - 1 ==
+                    GlobalLinkedPool::getPoolHeader(allocs[i]).sizeOfPool);
+        }
+    }
+}
